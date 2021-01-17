090b98a54e1b0e2e7cf5cc06fabf8811ee24cff5
Nadeen <nadeen@ualberta.ca>
2021-01-17 04:15:44 -0700

    added homepage and resolved issues with custom components

--- public/components/profile.js
+++ public/components/profile.js
@@ -1,42 +1,69 @@
-const template = document.createElement('template')
-template.innerHTML = `
-    <style>
-        .profile {
-            display: flex;
-            flex-direction: column;
-            flex-wrap: nowrap;
-            align-items: left;
-            background-color: #2b2d42;
-            border-radius: 1em;
-            padding: 2em;
-            margin: 2em;
-        }
-        .profile img {
-            width: 7em;
-            height: 7em;
-            border-radius: 50%;
-    </style>
-    <div class="profile">
-        <img alt="Profile Picture" class="profile-picture"/>
-        <div class="info">
-            <p><slot name="display-name" /></p>
-        </div>
-    </div>
-`
-
 class Profile extends HTMLElement {
-    constructor() {
-      super();
-
-      this.attachShadow({mode: 'open'});
-      this.shadowRoot.appendChild(template.content);
-      this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
-
-    }
-  
-    connectedCallback() {
-    }
+  constructor() {
+    super();
   }
-  
-  customElements.define("user-profile", Profile);
-  
+
+  connectedCallback() {
+    this.innerHTML = `
+                <div>
+                <style>
+                    .profile {
+                        display: flex;
+                        flex-direction: column;
+                        align-items: centre;
+                        justify-content: center;
+                        background-color: #2b2d42;
+                        border-radius: 1em;
+                        max-width: 175px;
+                        padding: 2em;
+                        margin: 2em;
+                        text-align: center;
+                    }
+                    .profile img {
+                        width: 7em;
+                        height: 7em;
+                        border-radius: 50%;
+                        object-fit: cover;
+                        margin-bottom: 1rem;
+                    }
+                    .display-name {
+                        text-align: center;
+                    }
+                    .skills {
+                      color: #DEDEDE;
+                      font-size: 0.8rem;
+                    }
+                </style>
+                <div class="profile">
+                    <img alt="Profile Picture" src=${
+                      this.hasAttribute("avatar")
+                        ? this.getAttribute("avatar")
+                        : "assets/blank_profile.jpg"
+                    }>
+                    <div class="info">
+                        <p class="display-name">${
+                          this.hasAttribute("display-name")
+                            ? this.getAttribute("display-name")
+                            : "Cool Panda"
+                        }</p>
+                        ${
+                          this.hasAttribute("user-attributes")
+                            ? this.getAttribute("user-attributes")
+                                .split(";")
+                                .reduce((accum, element) => {
+                                  element = element.split(",");
+                                  return (
+                                    accum +
+                                    `<p class="skills"><strong>${element[0]}<strong>: ${element[1]}</p>`
+                                  );
+                                }, "")
+                            : "<p class='skills'>Nothing to show (yet!)</p>"
+                        }
+                    </div>
+                </div>
+            </div>
+          `;
+  }
+}
+
+customElements.define("user-profile", Profile);
